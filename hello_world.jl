37d21939c14171031a72ef220a588d3c0f01a851
AMol Gawai <amol.gawai.biz@gmail.com>
2025-11-04 19:58:11 +0100

    Spelling

--- hello_world.jl
+++ hello_world.jl
@@ -16,23 +16,23 @@ else
 end
 
 # improvement 1
-divcheck = [number % 3 == 0, number % 5 == 0, number % 7 == 0]
+isDivisibleBy3_5_7 = [number % 3 == 0, number % 5 == 0, number % 7 == 0]
 # yesno = ["yes", "no"]
-println("$number is divisible by 3 - $(divcheck[1])")
-println("$number is divisible by 5 - $(divcheck[2])")
-println("$number is divisible by 7 - $(divcheck[3])")
+println("$number is divisible by 3 - $(isDivisibleBy3_5_7[1])")
+println("$number is divisible by 5 - $(isDivisibleBy3_5_7[2])")
+println("$number is divisible by 7 - $(isDivisibleBy3_5_7[3])")
 
-# print which quadrent in the cartesian plane the point is located
+# print which quadrant in the cartesian plane the point is located
 x, y = rand(-1.0:0.1:1.0, 2)
 # primitive way
 if x > 0 && y > 0
-    println("The point ($x, $y) lies in first quadrent")
+    println("The point ($x, $y) lies in first quadrant")
 elseif x < 0 && y > 0
-    println("The point ($x, $y) lies in second quadrent")
+    println("The point ($x, $y) lies in second quadrant")
 elseif x < 0 && y < 0
-    println("The point ($x, $y) lies in third quadrent")
+    println("The point ($x, $y) lies in third quadrant")
 elseif x > 0 && y < 0
-    println("The point ($x, $y) lies in fourth quadrent")
+    println("The point ($x, $y) lies in fourth quadrant")
 elseif x == 0 && y == 0
     println("The point ($x, $y) is at origin")
 else
