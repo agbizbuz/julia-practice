d9c624f42165c8e947586fcba841d9ba252476ce
Amol G Biz <amol.gawai.biz@gmail.com>
2026-02-09 16:07:24 +0100

    added script and project basics

--- Basics/hello_world.jl
+++ Basics/hello_world.jl
@@ -1,1 +1,20 @@
-println("Hello World!")
+# constants
+const LANG="DE"
+
+# functions
+function hello(name)
+    if LANG == "DE"
+        println("Hallo liebe/r $name !")
+    else
+        println("Hello dear $name !")
+    end
+end
+
+# main program
+function main()
+    hello("Peter")
+    hello("Jane")
+end
+
+main()
+nothing
